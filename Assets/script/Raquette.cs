5f6fd36ce566f6a1d74800b4b4d9aa264fd19acf
Gilles Avraam <gillouavraam@gmail.com>
2021-10-21 02:36:24 +0200

    update raquette

--- Assets/script/Raquette.cs
+++ Assets/script/Raquette.cs
@@ -6,20 +6,30 @@ public class Raquette : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 2f;
+    public float clampX = 10f;
 
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -clampX, clampX);
+        transform.position = pos;
+        transform.Translate(Vector3.right * h * Time.deltaTime * speed);
+
+
+        // Vector3 mouvement = new Vector3(pos.x, 0f, 0f);
+
+        //transform.position = pos * h;
+
         //float v = Input.GetAxis("Vertical");
 
-        Vector3 mouvement = new Vector3(h, 0f, 0f);
         //transform.position += Vector3.right * Time.deltaTime * speed;
-        transform.Translate(mouvement * Time.deltaTime  * speed);
+
+        //Debug.Log(mouvement);
     }
 }
